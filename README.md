5e0936a0187d830552c21ca14ec40bd37f389241
Stefan Niederhauser <ghuder5@gmx.ch>
2015-01-12 00:20:52 +0100

    add srcset and image tag

--- README.md
+++ README.md
@@ -12,4 +12,23 @@ The following options for `_config.yml` are available:
   - `raw_images` the directory containing the unoptimized images, default is `img/raw`.
   - `opt_images` the directory containing the optimized images, default is `img/opt`.
   - `images_link` the name of the symbolic link pointing to `opt_images`, default is `images`.
-  - `image_geometry` the size all images should be, is an ImageMagick [geometry string](http://www.imagemagick.org/RMagick/doc/imusage.html#geometry), default is `800x800>`.
+  - `image_geometry` the size all images should be, is an ImageMagick [geometry string](http://www.imagemagick.org/RMagick/doc/imusage.html#geometry), may be an array, default is `800x800>`.
+  - `image_layout` the file containing the template for the `image` tag, default is `_layouts/image.html`.
+
+The plugin defines two liquid tags: `srcset` and `image`.
+To include an image with multiple resolutions (using [scrset](http://ericportis.com/posts/2014/srcset-sizes/)) into a post, do the following:
+
+In `_layouts/image.html` define the template of an image. For example:
+```
+<img srcset="{% srcset {{image.index}} %}" alt="{{image.alt}}"/>
+```
+
+In the front matter of the post, add
+```
+image:
+    -   url: my_image.jpg
+        alt: My Alt
+```
+
+and reference the image using `{% image 0 %}` where 0 is the index of the image.
+
--- lib/jekyll/image_optimizer.rb
+++ lib/jekyll/image_optimizer.rb
@@ -1,4 +1,9 @@
 require_relative 'image_optimizer/image_optimizer'
+require 'RMagick'
+
+def opt_dir(config)
+  config['opt_images'] || 'img/opt'
+end
 
 module Jekyll
 
@@ -7,15 +12,66 @@ module Jekyll
 
     def initialize(config)
       @raw=config['raw_images'] || 'img/raw'
-      @opt=config['opt_images'] || 'img/opt'
+      @opt=opt_dir(config)
       @symlink=config['images_link'] || 'images'
       @geometry=config['image_geometry'] || '800x800>'
     end
 
     def generate(site)
       io=ImageOptimizer.new(@raw, @opt)
-      io.optimize_images(@geometry)
-      io.create_symlink(@symlink, @geometry)
+      if @geometry.is_a? Enumerable
+        @geometry.each { |geom| io.optimize_images(geom) }
+      else
+        io.optimize_images(@geometry)
+        io.create_symlink(@symlink, @geometry)
+      end
+    end
+  end
+
+  class SrcsetTag < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      super
+      @markup
+    end
+
+    def render(context)
+      s=''
+      base_url=context['site']['baseurl']
+      index=Liquid::Template.parse(@markup).render(context).to_i
+      src=context['page']['image'][index]['url']
+      for dir in Dir[opt_dir(context['site'])+'*']
+        file = dir+'/'+src
+        if File.file? file
+          img=Magick::Image::read(file).first
+          s+=base_url+'/'+file+' '+img.columns.to_s+'w,'
+        end
+      end
+      s[0..-2]
+    end
+  end
+
+  class ImageTag < Liquid::Tag
+
+    def initialize(tag_name, markup, tokens)
+      super
+      @index = markup.to_i
+    end
+
+    def render(context)
+      context.stack do
+        context['image']=context['page']['image'][@index]
+        context['image']['index']=@index
+        Liquid::Template.parse(File.read(layout(context))).render(context)
+      end
+    end
+
+    def layout(context)
+      context['site']['image_layout'] || '_layouts/image.html'
     end
   end
 end
+
+
+Liquid::Template.register_tag('srcset', Jekyll::SrcsetTag)
+Liquid::Template.register_tag('image', Jekyll::ImageTag)
